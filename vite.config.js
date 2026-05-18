4c492ed2b9b1f76adb70a317a373c534c53fcd3c
thetnaungsoe-mmocc <thetnaungsoe.mmocc@gmail.com>
2026-05-18 22:13:38 +0630

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/hnin-bd-wish/', 
+  base: '/happy-birthday-hnin/', 
   plugins: [react()],
 })
